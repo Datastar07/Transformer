5ae804aa59a04449a84f2aaf6308ec982da8d90b
Fardin <fardinmultani07@gmail.com>
2024-09-19 16:15:19 +0530

    add encoder block in model.

--- model.py
+++ model.py
@@ -137,6 +137,31 @@ class ResidualConnection(nn.Module):
 
 
 
+class EncoderBloack(nn.Module):
 
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_bloack = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_cooneections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
 
+    def forward(self, x, src_mask):
+        x = self.residual_cooneections[0](x, lambda x: self.self_attention_bloack(x,x,x,src_mask))
+        x= self.residual_cooneections[1](x, self.feed_forward_block)
+
+        return x
+
+class Encoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm  = LayerNormalization()
+
+    def forward(self, x ,mask):
+        for layer in self.layers:
+            x = layer(x,mask)
+            
+        return self.norm(x)
+    
 
