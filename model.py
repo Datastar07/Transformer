9bd79be9d2900415e2701f11cf29af019380c6f6
Fardin <fardinmultani07@gmail.com>
2024-09-19 15:38:37 +0530

    add residual block in the model.

--- model.py
+++ model.py
@@ -125,5 +125,18 @@ class MultiHeadAttentionBlock(nn.Module):
         return self.w_o(x)
 
 
+class ResidualConnection(nn.Module):
+
+    def __init__(self,dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+
+
 
 
