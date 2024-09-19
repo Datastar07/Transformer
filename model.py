f5ba3b96a08a879cc3e60943d259aae1efbb0d1b
Fardin <fardinmultani07@gmail.com>
2024-09-19 11:24:17 +0530

    add postional encodding part in transforemer model.

--- model.py
+++ model.py
@@ -12,3 +12,35 @@ class InputEmbeddings(nn.Module):
     
     def forward(self,x):
         return self.embedding(x) * math.sqrt(self.d_model)
+    
+
+class PositionalEmbeddings(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # create matrix of shape,(seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        # create a vector of shape(seq_len, 1)
+        position =  torch.arange(0, seq_len, dtype = torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+
+        # Apply sine term to even postion and cos term to odd postion,
+        pe[:,0::2]  = torch.sin(position * div_term)
+        pe[:,1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)  #(1,seq_len, d_model)
+
+        # not update this vector as backpropagation process,
+        self.register_buffer("pe", pe)
+
+    def forward(self,x):
+        x = x + (self.pe[:, :x.shape[1],:]).required_grad(False)
+        return self.dropout(x)
+
+
+        
