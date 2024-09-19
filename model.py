95212b7a11308b145e4830ef82fe5fc269bb80bb
Fardin <fardinmultani07@gmail.com>
2024-09-19 16:51:56 +0530

    add decoder block and last part of the model.

--- model.py
+++ model.py
@@ -161,7 +161,44 @@ class Encoder(nn.Module):
     def forward(self, x ,mask):
         for layer in self.layers:
             x = layer(x,mask)
-            
+
+        return self.norm(x)
+    
+class DecoderBlock(nn.Module):
+
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_network: FeedForwardBlock, dropout: nn.Dropout):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_network = feed_forward_network
+        self.residual_connection = nn.Module([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x= self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x,tgt_mask))
+        x= self.residual_connection[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x= self.residual_connection[2](x, self.feed_forward_network)
+
+        return x
+    
+class Decoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self,x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x= layer(x, encoder_output, src_mask,tgt_mask)
         return self.norm(x)
     
 
+class ProectionLayer(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        return torch.log_softmax(self.proj(x), dim = -1)
+    
