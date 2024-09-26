2d6ce4247fa1d2e2a21ce74fb6f4c50a41dca434
Fardin <fardinmultani07@gmail.com>
2024-09-26 15:28:47 +0530

    add some changes in dataset.py file.

--- dataset.py
+++ dataset.py
@@ -12,6 +12,7 @@ class BilingualDataset(Dataset):
         self.toknizer_tgt = toknizer_tgt
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
+        self.seq_len = seq_len
 
         self.sos_token = torch.Tensor([toknizer_src.token_to_id(['[SOS]'])], dtype = torch.int64)
         self.eos_token = torch.Tensor([toknizer_tgt.token_to_id(['[EOD]'])], dtype = torch.int64)
@@ -28,4 +29,54 @@ class BilingualDataset(Dataset):
         enc_input_tokens = self.toknizer_src.encode(src_text).ids
         dec_input_tokens = self.toknizer_tgt.decode(tgt_text).ids
 
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0 :
+            raise ValueError("sentence is too long.")
         
+        # Add SOS and EOS to the text,
+        encoder_input  = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token]*enc_num_padding_tokens, dtype= torch.int64)
+            ]
+        )
+
+        # Add SOS decoder input,
+        decoder_input  = torch.cat(
+            [
+                self.eos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token]*dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        # Add EOS to the label (what we expect as ouput from the decoder),
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensror([self.pad_token]*dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+         
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        return {
+            "encoder_input": encoder_input, # seq_len
+            "decoder_input":decoder_input, # seq_len
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), #(1,1,seq_len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unqueeze(0).int() & causal_mask(decoder_input.size(0)), # (1,seq_len) & (1_seq_len, seq_len)
+            "label": label, # (seq_len)
+            "src_text": src_text,
+            "tgt_text": tgt_text
+        }
+    
+def causal_mask(size):
+    mask = torch.triu(torch.ones(1,size,size), diagonal=1).type(torch.int)
+    return mask == 0
